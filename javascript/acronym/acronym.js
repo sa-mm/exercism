b03439d09455fd41ff7229d51fdbea5e0dd70644
blah blah <sa-mm@users.noreply.github.com>
2017-02-20 13:06:31 -0600

    Scrabble

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,8 +1,8 @@
 Acronyms = {
   parse: function (str) {
-    var colonIndex = str.search(/:/)
+    var colonIndex = str.search(/:/);
     if (colonIndex) {
-      str = str.slice(0,colonIndex);
+      str = str.slice(0, colonIndex);
     }
     var re = /\s|\-/g;
     var arr = str.split(re).map(function (word) {
